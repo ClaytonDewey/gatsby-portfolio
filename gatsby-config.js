c66e134996b19e2857fadb337b36265169d7d8eb
Clayton Dewey <clay@dryadmedia.com>
2022-10-15 14:41:25 -0500

    Add plugins to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,23 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
 module.exports = {
-  siteMetadata: {
-    title: `gatsby-portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-styled-components"]
+  /* Your site config here */
+
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
 };
