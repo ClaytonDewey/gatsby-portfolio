824885ac21676d160e2369e2e8d68818361a0d56
Clayton Dewey <clay@dryadmedia.com>
2022-10-15 17:35:47 -0500

    Change gatsby config & add .env to .gitignore

---
+++ gatsby.config.ts
@@ -0,0 +1,34 @@
+import type { GatsbyConfig } from "gatsby";
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL,
+  accessToken: process.env.STRAPI_TOKEN,
+  collectionTypes: ["job"],
+  singleTypes: [],
+};
+
+const config: GatsbyConfig = {
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-strapi`,
+      options: strapiConfig,
+    },
+  ],
+};
+
+export default config;
